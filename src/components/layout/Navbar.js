f6806460263555a2982d9f5bc4845f074dc08cca
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-03-22 13:06:21 +0300

    implemented signed in and out links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 function Navbar() {
   return (
     <nav className='nav-wrapper purple darken-3'>
       <div className='container'>
-        <Link to='/' className='left brand-logo'>
+        <Link to='/' className='left  brand-logo'>
           K-Crt
         </Link>
+        <SignedInLinks />
+        <SignedOutLinks />
       </div>
     </nav>
   );
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function SignedInLinks() {
+  return (
+    <div>
+      <ul className='right'>
+        <li>
+          <NavLink to='/'>New Project</NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>Log Out</NavLink>
+        </li>
+        <li>
+          <NavLink to='/' className='btn btn-floating red lighten-1'>
+            KM
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SignedInLinks;
