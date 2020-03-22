f24dd23e3df202066eb03137fabdb5240ebb0a1f
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-03-22 12:30:36 +0300

    added react router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='container'>
-      <h1>MArio plan</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Ken Plan</h1>
+      </div>
+    </Router>
   );
 }
 
