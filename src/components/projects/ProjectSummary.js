518701afb801bc5d7751f304b1ae86ad258cdab1
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-03-22 13:37:32 +0300

    added project summary functionality

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div>
+      <div className='card z-depth-0 project-summary yellow lighten-4'>
+        <div className='card-content grey-text text-darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by Ken Mugy</p>
+          <p className='grey-text'>21 march</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
