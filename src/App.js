4223b228745f3ae2a6e05436ffaf1d2d981426ec
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-03-22 13:22:32 +0300

    implemented dashboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
       <>
         <Navbar />
+        <Switch>
+          <Route path='/'>
+            <Dashboard />
+          </Route>
+        </Switch>
       </>
     </Router>
   );
